65dc9e5cda7dd82f49ac67a2f1ce38d043fd3132
Easante2 <e.asante@hotmail.com>
2021-03-07 21:15:20 +0000

    Push C# project solution for Future Farm test

---
+++ FutureFarmTechTest/AppleTreeFarm.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using System.Collections;
+using System.Linq;
+
+namespace FutureFarmTechTest
+{
+    //Enum of Chemicals and price
+    public enum Chemicals{
+        Ryezapon = 240,
+        Berbelikar = 156,
+        Quadranis = 191
+    }
+    public class AppleTreeFarm
+    {
+
+        public AppleTreeFarm() { }
+
+        //second constractor to create new farm field 
+        public AppleTreeFarm(string name, int width, int length, string crop, DateTime lastSprayed, int applFrequency)
+        {
+            Name = name;
+            Width = width;
+            Length = length;
+            Crop = crop;
+            LastSprayed = lastSprayed;
+            ApplicationFrequency = applFrequency;
+            Area = width * length;
+        }
+
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public string Crop { get; set; }
+        public DateTime LastSprayed { get; set; }
+        public int ApplicationFrequency { get; set; }
+        public DateTime NextOrderDate { get; set; }
+        public double TotalCost { get; set; }
+        //Area of the farm field is width x length. Area unit is squared meters 
+        public int Area { get; set; }
+        
+        public List<AppleTreeFarm> farmField = new List<AppleTreeFarm>();
+        
+        //Use Date and calendar library to calculate date for next order
+        public void CalcWeeksTillNextOrder()
+        {
+            for (int i =0; i < farmField.Count; i++) { 
+                Calendar myCal = CultureInfo.InvariantCulture.Calendar;
+                DateTime dt = farmField[i].LastSprayed;
+                //Add 6 weeks to last sprayed date
+                dt = myCal.AddWeeks(farmField[i].LastSprayed, farmField[i].ApplicationFrequency);
+                //minus by one week as order will need to be placed a week before
+                dt = myCal.AddWeeks(dt, -1);
+                farmField[i].NextOrderDate = dt;
+                System.Diagnostics.Debug.WriteLine(dt);
+            }
+        }
+
+        //Method to calculate the cost per litre for each chemical order
+        public void CalcCostPerChemical()
+        {
+            //Loop through each Enum of chemicals
+            //Find price per litre for each chemical for each farm field
+            foreach(int pricePerLitre in Enum.GetValues(typeof(Chemicals)))
+            {
+                for (int i = 0; i < farmField.Count; i++) { 
+                    int litre = 0;
+                    string chemical = Enum.GetName(typeof(Chemicals), pricePerLitre);
+                    System.Diagnostics.Debug.WriteLine(pricePerLitre + " " + chemical);
+                
+                    //looping through each chemical and calculate cost
+                    if(chemical == "Ryezapon" && farmField[i].Crop == "Winter Wheat")
+                    {
+                        //since 1 litre of a given chemical covers 1 Hectare, litre = hectare
+                         litre = ConvertAreaSqMetersToHectare(farmField[i].Area);
+                        //find the total cost by multiplying the price of the chemical by the number of litres
+                        farmField[i].TotalCost = Math.Round(Convert.ToDouble(pricePerLitre * litre), 2);
+                    } 
+                    else if(chemical == "Berbelikar" && farmField[i].Crop == "Barley")
+                    {
+                        litre = ConvertAreaSqMetersToHectare(farmField[i].Area);
+                        farmField[i].TotalCost = Math.Round(Convert.ToDouble(pricePerLitre * litre), 2);
+                    } 
+                    else if(chemical == "Quadranis" && farmField[i].Crop == "Sugar Beet")
+                    {
+                        litre = ConvertAreaSqMetersToHectare(farmField[i].Area);
+                        farmField[i].TotalCost = Math.Round(Convert.ToDouble(pricePerLitre * litre), 2);
+                    }
+                }
+            }
+        }
+
+        //1 hectare = 10,000 squared meters
+        //the area of the farm field is width x length in squared meters
+        //To find the hectare of the farm field = Area / 10000
+        public int ConvertAreaSqMetersToHectare(int area)
+        {
+            int hectareInSqMeters = 10000;
+            return area / hectareInSqMeters;
+        }
+
+        //Use Params to store multiple objects, from method call, into a temp array and add each object into an arraylist
+        //Add each farm field to an arraylist
+        public void AddFarmFieldToArrayList(params AppleTreeFarm[] field)
+        {
+            farmField.Clear();
+            foreach(AppleTreeFarm f in field)
+            {
+                farmField.Add(f);
+            }
+            
+        }
+
+        //Output the order catalogue
+        public void DisplayOrderCatalogue()
+        {
+            //Call the methods to calculate the next order dates and price per chemical 
+            CalcWeeksTillNextOrder();
+            CalcCostPerChemical();
+
+            Console.WriteLine("Order Catalogue list:");
+            for(int i =0; i < farmField.Count; i++) {
+                if (farmField[i].Crop == "Winter Wheat") { 
+                    Console.WriteLine("Next order date: " + farmField[i].NextOrderDate.ToString("dd/MM/yyyy") +  " , " +
+                        "Chemical: " + Enum.GetName(typeof(Chemicals), 240) + " , " + "Price: £" +  farmField[i].TotalCost);
+                }
+                else if (farmField[i].Crop == "Barley")
+                {
+                    Console.WriteLine("Next order date: " + farmField[i].NextOrderDate.ToString("dd/MM/yyyy") + " , " +
+                        "Chemical: " + Enum.GetName(typeof(Chemicals), 156) + " , " + "Price: £" + farmField[i].TotalCost);
+                }  
+                else if (farmField[i].Crop == "Sugar Beet")
+                {
+                    Console.WriteLine("Next order date: " + farmField[i].NextOrderDate.ToString("dd/MM/yyyy") + " , " +
+                        "Chemical: " + Enum.GetName(typeof(Chemicals), 191) + " , " + "Price: £" + farmField[i].TotalCost);
+                }
+            }
+        }
+
+    }
+
+
+
+
+
+
+}
---
+++ FutureFarmTechTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FutureFarmTechTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new instance of the farm and define each farm field
+            AppleTreeFarm newFarmField1 = new AppleTreeFarm("Cowfield", 300, 400, "Barley", Convert.ToDateTime("18 / 02 / 2020"), 4);
+            AppleTreeFarm newFarmField2 = new AppleTreeFarm("Hillside", 600, 400, "Winter Wheat", Convert.ToDateTime("05 / 02 / 2020"), 6);
+            AppleTreeFarm newFarmField3 = new AppleTreeFarm("Top", 200, 600, "Barley", Convert.ToDateTime("22 / 12 / 2020"), 4);
+            AppleTreeFarm newFarmField4 = new AppleTreeFarm("Big Field", 800, 500, "Sugar Beet", Convert.ToDateTime("09 / 01 / 2020"), 9);
+            AppleTreeFarm newFarmField5 = new AppleTreeFarm("Upper River", 200, 200, "Barley", Convert.ToDateTime("18 / 01 / 2020"), 4);
+            AppleTreeFarm newFarmField6 = new AppleTreeFarm("Lower River", 500, 100, "Sugar Beet", Convert.ToDateTime("08 / 02 / 2020"), 9);
+            AppleTreeFarm newFarmField7 = new AppleTreeFarm("Southside", 400, 400, "Winter Wheat", Convert.ToDateTime("12 / 01 / 2020"), 6);
+            AppleTreeFarm farm = new AppleTreeFarm();
+            farm.AddFarmFieldToArrayList(newFarmField1, newFarmField2, newFarmField3, newFarmField4, newFarmField5, newFarmField6, newFarmField7);
+            farm.DisplayOrderCatalogue();
+          
+        }
+    }
+}
